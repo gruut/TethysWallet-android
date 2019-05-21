27c90d3ccf0b92d1d3938cc2e6d0b674b385539e
Minyoung <song@thevaulters.com>
2019-05-21 17:13:15 +0900

    # ADD - Validate Merger Setup Inputs
    * IP Address - mandatory, IP address regex check
    * Port Number - mandatory, only numbers
    * Password - mandatory, minimum length is 14
    * Retype Password - mandatory, minimum length is 14, equal with password input
    
    * Validation check before click OK button

--- app/src/main/java/com/veronnnetworks/veronnwallet/ui/merger/MergerFragment.kt
+++ app/src/main/java/com/veronnnetworks/veronnwallet/ui/merger/MergerFragment.kt
@@ -5,18 +5,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-
+import com.veronnnetworks.veronnwallet.R
 import com.veronnnetworks.veronnwallet.databinding.FragmentMergerBinding
 import com.veronnnetworks.veronnwallet.model.MergerInfo
 import com.veronnnetworks.veronnwallet.ui.NavigationController
 import com.veronnnetworks.veronnwallet.ui.common.fragment.BaseFragment
+import com.veronnnetworks.veronnwallet.ui.common.view.ValidatableView
+import com.veronnnetworks.veronnwallet.ui.common.view.validators.*
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class MergerFragment : BaseFragment() {
-
     private lateinit var binding: FragmentMergerBinding
+    private val validatableViews: ArrayList<ValidatableView> = ArrayList()
+    private val compositeDisposable = CompositeDisposable()
 
     @Inject
     lateinit var navigationController: NavigationController
@@ -39,12 +49,84 @@ class MergerFragment : BaseFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        initValidators()
         binding.mergerInfo = MergerInfo()
-        binding.ok.setOnClickListener {
-            mergerViewModel.sendUserInfo(binding.mergerInfo!!)
+        binding.container.setOnClickListener { hideKeyboard() }
+        binding.ok.setOnClickListener(this::onOkClick)
+        binding.mergerPasswordCheck.setOnEditorActionListener { _, actionId, _ ->
+            when (actionId) {
+                EditorInfo.IME_ACTION_DONE -> {
+                    hideKeyboard()
+                    onOkClick(null)
+                    return@setOnEditorActionListener true
+                }
+                else -> return@setOnEditorActionListener false
+            }
         }
 
         lifecycle.addObserver(mergerViewModel)
+    }
+
+    private fun initValidators() {
+        val minLength = 14
+        validatableViews.addAll(
+            arrayOf(
+                binding.mergerPortLayout.register(
+                    listOf(
+                        RequiredValidator(getString(R.string.validation_err_required)),
+                        NumberOnlyValidator(getString(R.string.validation_err_number_only))
+                    )
+                ),
+                binding.mergerIpLayout.register(
+                    listOf(
+                        RequiredValidator(getString(R.string.validation_err_required)),
+                        IpValidator(getString(R.string.validation_err_ip))
+                    )
+                ),
+                binding.mergerPasswordLayout.register(
+                    listOf(
+                        RequiredValidator(getString(R.string.validation_err_required)),
+                        MinLengthValidator(
+                            getString(R.string.validation_err_minimum, minLength),
+                            minLength
+                        )
+                    )
+                ),
+                binding.mergerPasswordCheckLayout.register(
+                    listOf(
+                        RequiredValidator(getString(R.string.validation_err_required)),
+                        MinLengthValidator(
+                            getString(R.string.validation_err_minimum, minLength),
+                            minLength
+                        ),
+                        RetypeValidator(
+                            getString(R.string.validation_err_not_matched),
+                            binding.mergerPassword
+                        )
+                    )
+                )
+            )
+        )
+    }
+
+    private fun onOkClick(@Suppress("UNUSED_PARAMETER") view: View?) {
+        val validations = validatableViews.map { it.validateAsCompletable() }
+        compositeDisposable.clear()
+        compositeDisposable.add(
+            Completable.mergeDelayError(validations)
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onComplete = { mergerViewModel.sendUserInfo(binding.mergerInfo!!) },
+                    onError = {
+                        Toast.makeText(
+                            activity,
+                            R.string.validation_error_occurred,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                )
+        )
     }
 
     companion object {
--- app/src/main/java/com/veronnnetworks/veronnwallet/ui/merger/MergerViewModel.kt
+++ app/src/main/java/com/veronnnetworks/veronnwallet/ui/merger/MergerViewModel.kt
@@ -1,17 +1,24 @@
 package com.veronnnetworks.veronnwallet.ui.merger
 
 import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.veronnnetworks.veronnwallet.auth.KeyStoreHelper
 import com.veronnnetworks.veronnwallet.data.grpc.GrpcService
 import com.veronnnetworks.veronnwallet.data.grpc.message.request.MsgSetupMerger
 import com.veronnnetworks.veronnwallet.model.MergerInfo
+import com.veronnnetworks.veronnwallet.ui.Result
 import com.veronnnetworks.veronnwallet.ui.common.mapper.toResult
+import com.veronnnetworks.veronnwallet.utils.ext.map
 import com.veronnnetworks.veronnwallet.utils.rx.SchedulerProvider
+import com.veronnworks.veronnwallet.Reply
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.rxkotlin.zipWith
+import timber.log.Timber
+import java.io.InvalidObjectException
 import javax.inject.Inject
 
 class MergerViewModel @Inject constructor(
@@ -20,22 +27,32 @@ class MergerViewModel @Inject constructor(
 ) : ViewModel(), LifecycleObserver {
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
-    fun sendUserInfo(mergerInfo: MergerInfo) {
-        if (!mergerInfo.ip.isNullOrEmpty() && !mergerInfo.port.isNullOrEmpty()) {
-            val grpcService =
-                GrpcService(mergerInfo.ip!!, mergerInfo.port!!.toInt(), schedulerProvider)
+    private val mutableReply: MutableLiveData<Result<Reply>> = MutableLiveData()
+    val reply: LiveData<Result<Reply>> = mutableReply
 
-            keyStoreHelper.getCertificatePem()
-                .zipWith(keyStoreHelper.getEncryptedSecretKeyPem("test")) { cert: String, sk: String? ->
-                    sk?.let { MsgSetupMerger(it, cert) }
-                }
-                .flatMap { msg: MsgSetupMerger -> grpcService.userService(msg) }
-                .toResult(schedulerProvider)
-                .subscribeBy {
-                    // TODO: user result
-                }
-                .addTo(compositeDisposable)
-        }
+    val isLoading: LiveData<Boolean> by lazy {
+        reply.map { it.inProgress }
+    }
+
+    fun sendUserInfo(mergerInfo: MergerInfo) {
+        keyStoreHelper.getCertificatePem()
+            .zipWith(keyStoreHelper.getEncryptedSecretKeyPem("test")) { cert: String, sk: String? ->
+                sk?.let { MsgSetupMerger(it, cert) }
+            }
+            .flatMap { msg: MsgSetupMerger ->
+                if (!mergerInfo.validate()) throw InvalidObjectException("Invalid inputs")
+                GrpcService(
+                    mergerInfo.ip!!,
+                    mergerInfo.port!!.toInt(),
+                    schedulerProvider
+                ).userService(msg)
+            }
+            .toResult(schedulerProvider)
+            .subscribeBy(
+                onError = { Timber.e(it) },
+                onNext = { mutableReply.value = it }
+            )
+            .addTo(compositeDisposable)
     }
 
     override fun onCleared() {
