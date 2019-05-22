ed527b7a8c576afea049c7b405eb6d61d7a66424
Minyoung <song@thevaulters.com>
2019-05-22 15:32:54 +0900

    # ADD - rxJava error handler
    * 처리되지 못하고 남아있는 rx 에러를 일괄로 핸들링

--- app/src/main/java/com/veronnnetworks/veronnwallet/ui/BaseApp.kt
+++ app/src/main/java/com/veronnnetworks/veronnwallet/ui/BaseApp.kt
@@ -5,7 +5,11 @@ import com.veronnnetworks.veronnwallet.di.component.DaggerAppComponent
 import com.veronnnetworks.veronnwallet.di.module.NetworkModule
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
 import timber.log.Timber
+import java.io.IOException
+import java.net.SocketException
 
 class BaseApp : DaggerApplication() {
 
@@ -13,11 +17,42 @@ class BaseApp : DaggerApplication() {
         super.onCreate()
 
         setupTimber()
+        setupErrorHandler()
     }
 
     private fun setupTimber() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    private fun setupErrorHandler() {
+        RxJavaPlugins.setErrorHandler { e ->
+            var error = e
+            if (error is UndeliverableException) {
+                error = e.cause
+            }
+            if (error is IOException || error is SocketException) {
+                // fine, irrelevant network problem or API that throws on cancellation
+                return@setErrorHandler
+            }
+            if (error is InterruptedException) {
+                // fine, some blocking code was interrupted by a dispose call
+                return@setErrorHandler
+            }
+            if (error is NullPointerException || error is IllegalArgumentException) {
+                // that's likely a bug in the application
+                Thread.currentThread().uncaughtExceptionHandler
+                    .uncaughtException(Thread.currentThread(), error)
+                return@setErrorHandler
+            }
+            if (error is IllegalStateException) {
+                // that's a bug in RxJava or in a custom operator
+                Thread.currentThread().uncaughtExceptionHandler
+                    .uncaughtException(Thread.currentThread(), error)
+                return@setErrorHandler
+            }
+            Timber.w(error, "Undeliverable exception received, not sure what to do")
         }
     }
 
