bf0f1464ced201d0077f172a21fc9660696eec34
Minyoung <song@thevaulters.com>
2019-05-22 15:31:55 +0900

    # ADD - MergerActivity Toolbar setup
    * MainActivity를 Parent로 하여 뒤로가기 구현

--- app/src/main/java/com/veronnnetworks/veronnwallet/ui/merger/MergerActivity.kt
+++ app/src/main/java/com/veronnnetworks/veronnwallet/ui/merger/MergerActivity.kt
@@ -3,7 +3,9 @@ package com.veronnnetworks.veronnwallet.ui.merger
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.veronnnetworks.veronnwallet.R
+import com.veronnnetworks.veronnwallet.databinding.ActivityMergerBinding
 import com.veronnnetworks.veronnwallet.ui.NavigationController
 import com.veronnnetworks.veronnwallet.ui.common.activity.BaseActivity
 import javax.inject.Inject
@@ -13,9 +15,16 @@ class MergerActivity : BaseActivity() {
     @Inject
     lateinit var navigationController: NavigationController
 
+    private val binding: ActivityMergerBinding by lazy {
+        DataBindingUtil
+            .setContentView<ActivityMergerBinding>(this, R.layout.activity_merger)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_merger)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setTitle(R.string.nav_item_merger)
 
         navigationController.navigateToMerger()
     }
