190d32575a02c57a39cdf51602d91164aedcaea3
Minyoung <song@thevaulters.com>
2019-06-10 10:50:17 +0900

    # BUGFIX - Signer Setup에서 Back button 적용되도록 수정

--- app/src/main/java/io/tethys/tethyswallet/ui/signer/SignerFragment.kt
+++ app/src/main/java/io/tethys/tethyswallet/ui/signer/SignerFragment.kt
@@ -4,14 +4,19 @@ import android.content.Context
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import io.tethys.tethyswallet.R
+import io.tethys.tethyswallet.ui.common.fragment.IOnBackPressed
 import io.tethys.tethyswallet.utils.AppConstants
 
-class SignerFragment : PreferenceFragmentCompat() {
+class SignerFragment : PreferenceFragmentCompat(), IOnBackPressed {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         preferenceManager.sharedPreferencesName = AppConstants.PREF_NAME
         preferenceManager.sharedPreferencesMode = Context.MODE_PRIVATE
 
         setPreferencesFromResource(R.xml.signer_setup_pref, rootKey)
+    }
+
+    override fun onBackPressed(): Boolean {
+        return false
     }
 
     companion object {
